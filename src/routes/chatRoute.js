088e83204e9fcb395e6df10a63f0ecb427dc2186
nitish0104 <nitishdalvi1@gmail.com>
2023-05-29 17:58:38 +0530

    all chat routes

--- src/routes/chatRoute.js
+++ src/routes/chatRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { accessChat, fetchChats } = require("../controller/chatControllers");
+const { accessChat, fetchChats, createGroupChat, renameGroup, removeFromGroup, addToGroup } = require("../controller/chatControllers");
 const auth = require('../middleware/auth')
 const chatRouter = express.Router()
 
@@ -9,9 +9,9 @@ chatRouter
 	.post(auth, accessChat)
 
 chatRouter.route("/").get(auth, fetchChats);
-// chatRouter.route("/group").post(auth, createGroupChat);
-// chatRouter.route("/rename").put(auth, renameGroup);
-// chatRouter.route("/groupremove").put(auth, removeFromGroup);
-// chatRouter.route("/groupadd").put(auth, addToGroup);
+chatRouter.route("/group").post(auth, createGroupChat);
+chatRouter.route("/rename").put(auth, renameGroup);
+chatRouter.route("/groupremove").put(auth, removeFromGroup);
+chatRouter.route("/groupadd").put(auth, addToGroup);
 
 module.exports = chatRouter;
